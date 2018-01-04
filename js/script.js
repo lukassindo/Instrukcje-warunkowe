9a5a0994e0c9b89a52b6bba09d5fd145efa4c7ea
Lukasz Trzaska <lukasztrzaska81@gmail.com>
2018-01-04 11:34:03 +0100

    poprawki

--- js/script.js
+++ js/script.js
@@ -1,23 +1,22 @@
-var a = 4;
-var b = 7;
+var a = prompt('Enter value a');
+var b = prompt('Enter value b');
 var value = (a * a) - (2 * a * b) + (b * b);
 
 var elValue = document.getElementById('result');
-elValue.innerHTML = value;
+elValue.textContent = value;
 console.log(value);
 
 var msg;
 
 if (value > 0) {
 	msg = 'Jest to wynik dodatni';
-	console.log('Jest to wynik dodatni');
 } else if (value < 0) {
 	msg = 'Jest to wynik ujemny';
-	console.log('Jest to wynik ujemny');
 } else {
     msg = 'Jest to wynik zerowy';
-    console.log('Jest to wynik zerowy');
 }
+
+console.log(msg);
 
 var el = document.getElementById('plusorminus');
 el.textContent = msg;
